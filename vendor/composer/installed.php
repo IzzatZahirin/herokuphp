5e2cadc4cb14819f2ad9c5b48f17af5ca6289671
IzzatZahirin <20042321@myrp.edus.sg>
2022-07-08 16:00:52 +0800

    initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor_name/heroku',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '728960cf36f2430581e023ba19a4614543439a27',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor_name/heroku' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '728960cf36f2430581e023ba19a4614543439a27',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
