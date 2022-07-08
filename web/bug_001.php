db9d49c91bf5275718bbe584324f9c236acad1c1
IzzatZahirin <20042321@myrp.edus.sg>
2022-07-08 15:16:55 +0800

    initial commit

---
+++ web/bug_001.php
@@ -0,0 +1,12 @@
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>20-week training schedule for your first marathon</title>
+</head>
+
+<body>
+    Hello World!
+</body>
+
+</html>
