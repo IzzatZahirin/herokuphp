75f49f86dd0f080545837df7e4b27032fd591b38
IzzatZahirin <20042321@myrp.edus.sg>
2022-07-08 15:53:34 +0800

    initial commit

---
+++ web/bug_004_fixed.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Put your description here.">
+    <title>20-week training schedule for your first marathon</title>
+</head>
+
+<body>
+    Hello World!
+    <img src="img/wallpaper.jpg" alt="Hills by the waves">
+</body>
+
+</html>
